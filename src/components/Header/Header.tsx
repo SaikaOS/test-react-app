48336c3f719c6d663dbcf5a74edb35423042e8f6
SaikaOS <200103235@stu.sdu.edu.kz>
2022-08-02 11:24:28 +0600

    adaptive iphone se

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,13 +15,18 @@ const Header:FC<HeaderType> = (user: HeaderType) => {
   }
   return (
     <header className={styles.header}>
-        <img src="src\assets\logo.svg" alt="logo" className={styles.header__logo}/>
+      {window.innerWidth <= 375 ? (
+        <img src="src\assets\Mob-header.jpg" alt="mobile" />
+      )
+    : <img src="src\assets\logo.svg" alt="logo" className={styles.header__logo}/>}
         <div className={styles.header__block_logged}>
           <p className={styles.header__block_logged__username}>{user.username}</p>
           {user.isLoggedIn && (
             <Link to='/'>
+            {window.innerWidth <= 375 ? '' : (
               <img className={styles.header__logout} src="src\assets\logOut.svg" alt="log-out"/>
-            </Link>
+            )}
+          </Link>
           )}
         </div>
     </header>
